554bba668377efe6a8dd6e86fd7bdf60d8c53125
Augusto Zvoboter <zvoboter@gmail.com>
2016-10-27 20:58:32 -0200

    Classes de repositório para hibernate e configuração do xml

---
+++ SotosWoodwork/SotosWoodwork/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace SotosWoodwork.Repository
+{
+    interface IRepository
+    {
+        void Save(object obj);
+        void Delete(object obj);
+        object GetById(Type objType, object objId);
+        IQueryable<T> ToList<T>();
+    }
+}
